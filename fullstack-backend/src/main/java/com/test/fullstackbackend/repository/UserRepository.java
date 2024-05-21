c78966e8c0b47a76bf35ea7e05a0446732d39b24
red2922 <inthavongsaj@lopers.unk.edu>
2024-05-20 21:12:50 -0500

    Completely redone Springboot

--- fullstack-backend/src/main/java/com/test/fullstackbackend/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.test.fullstackbackend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.test.fullstackbackend.model.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
