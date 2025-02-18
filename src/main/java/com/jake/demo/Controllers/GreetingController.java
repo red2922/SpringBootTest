f9df42fde4bb8ce77ae0dcbc02443534392d86b6
Jake Inthavongsa <jinthavongsa1@gmail.com>
2025-02-18 15:18:30 -0600

    Added some basic endpoints

--- src/main/java/com/jake/demo/Controllers/GreetingController.java
+++ src/main/java/com/jake/demo/Controllers/GreetingController.java
@@ -2,12 +2,36 @@ package com.jake.demo.Controllers;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @RestController
+@RequestMapping("/api")
+
 public class GreetingController {
 
-    @GetMapping("/greetings")
-    public String getMethodName() {
+    @GetMapping("/greeting")
+    @ResponseBody
+    public String getGreeting() {
         return "Hello";
+    }
+
+    @GetMapping("/greeting/{name}")
+    @ResponseBody
+    public String getGreetingUser(@PathVariable String name) {
+        return "Hello " + name;
+    }
+
+    @GetMapping("/goodbye")
+    @ResponseBody
+    public String getGoodbye() {
+        return "Goodbye";
+    }
+
+    @GetMapping("/goodbye/{name}")
+    @ResponseBody
+    public String getGoodbyeUser(@PathVariable String name) {
+        return "Goodbye " + name;
     }
 }
